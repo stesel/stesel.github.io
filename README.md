ed9b87bee998858e701d24e215a4d0fe372845b2
Leonid Trofymchuk <stesel23@gmail.com>
2019-03-30 13:05:43 +0200

    Remove font awasome

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stesel.github.io
 Landing Page
+
+TODO
+https://www.w3schools.com/css/css_tooltip.asp
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import "./Home.css";
-import "@fortawesome/fontawesome-free/css/all.css";
+import { GitHubIcon } from "../icons/GitHubIcon";
+import { LinkedinIcon } from "../icons/LinkedinIcon";
+import { TwitterIcon } from "../icons/TwitterIcon";
+import {MediumIcon} from "../icons/MediumIcon";
+import { CodesandboxIcon } from "../icons/CodesandboxIcon";
 
 export const Home: React.FunctionComponent = () => {
     return (
@@ -8,11 +12,11 @@ export const Home: React.FunctionComponent = () => {
             <div className="intro">Hello, I'm Leonid!</div>
             <div className="tagline">CODING | MUSIC | GAMING</div>
             <div className="icons">
-                <a target="_blank" href="https://github.com/stesel"><i className="fab fa-github"></i></a>
-                <a target="_blank" href="https://www.linkedin.com/in/leonid-trofymchuk-2a976460"><i className="fab fa-linkedin"></i></a>
-                <a target="_blank" href="https://twitter.com/Stesel23"><i className="fab fa-twitter"></i></a>
-                <a target="_blank" href="https://medium.com/@stesel23"><i className="fab fa-medium"></i></a>
-                <a target="_blank" href="https://codesandbox.io/u/stesel"><i className="fab fa-codepen"></i></a>
+                <a target="_blank" href="https://github.com/stesel"><GitHubIcon svgClassName="icon" pathClassName="path" /></a>
+                <a target="_blank" href="https://www.linkedin.com/in/leonid-trofymchuk-2a976460"><LinkedinIcon  svgClassName="icon" pathClassName="path" /></a>
+                <a target="_blank" href="https://twitter.com/Stesel23"><TwitterIcon svgClassName="icon" pathClassName="path" /></a>
+                <a target="_blank" href="https://medium.com/@stesel23"><MediumIcon svgClassName="icon" pathClassName="path" /></a>
+                <a target="_blank" href="https://codesandbox.io/u/stesel"><CodesandboxIcon svgClassName="icon" pathClassName="path" /></a>
             </div>
         </div>
     );
