a1d131464026e2a8f1b060a1692f216e9d197997
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-26 13:28:10 +0000

    Deploy stesel/stesel.github.io to github.com/stesel/stesel.github.io.git:master

---
+++ precache-manifest.4bae78b16913cf6c4a0754bed3d41ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c920267f045ffc19a7ab7a55b44006a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fafed483e05a1af8d381",
+    "url": "/static/css/main.d14bfcc4.chunk.css"
+  },
+  {
+    "revision": "2c775ef923fbda4f2cfe",
+    "url": "/static/js/2.1c896b76.chunk.js"
+  },
+  {
+    "revision": "fafed483e05a1af8d381",
+    "url": "/static/js/main.38401165.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
