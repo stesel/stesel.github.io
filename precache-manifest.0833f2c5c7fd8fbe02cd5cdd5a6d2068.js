cfb2783c9d0857990520ba3bf40a34b8d9f8af3f
Leonid Trofymchuk <ltrofymchuk@evolutiongaming.com>
2020-02-29 20:50:35 +0200

    Updates

---
+++ precache-manifest.0833f2c5c7fd8fbe02cd5cdd5a6d2068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee76f9da61dfb4ecba43bb04d83b5396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181ef8958e994cbe23fc",
+    "url": "/static/css/main.d14bfcc4.chunk.css"
+  },
+  {
+    "revision": "d145d06f23a2eb7922e2",
+    "url": "/static/js/2.f26d5c55.chunk.js"
+  },
+  {
+    "revision": "f5564099d4817ac24914f319c813d6d9",
+    "url": "/static/js/2.f26d5c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181ef8958e994cbe23fc",
+    "url": "/static/js/main.696bbcc8.chunk.js"
+  },
+  {
+    "revision": "c83e11043bd1bb4ea137",
+    "url": "/static/js/runtime-main.85cd265a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4bae78b16913cf6c4a0754bed3d41ab6.js"
+  "/precache-manifest.0833f2c5c7fd8fbe02cd5cdd5a6d2068.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
